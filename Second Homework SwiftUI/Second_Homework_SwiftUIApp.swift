c740549bc63f138ccc0e5b10e070299dbf71ec18
Pavlin Dimitrov <pavlin.dimitrov@paysafe.com>
2022-11-06 18:53:31 +0200

    Initial Commit

---
+++ Second Homework SwiftUI/Second_Homework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_Homework_SwiftUIApp.swift
+//  Second Homework SwiftUI
+//
+//  Created by Pavlin Dimitrov on 6.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Second_Homework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
